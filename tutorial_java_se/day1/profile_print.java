433f70791266fab68f286da5e36837d95df965e3
shengzhen <fushengzhen@163.com>
2019-05-09 16:47:35 +0800

    initial commit

---
+++ tutorial_java_se/day1/profile_print.java
@@ -0,0 +1,58 @@
+package day1;
+
+import java.util.Scanner;
+
+
+public class profile_print {
+    public String name;
+    public String country;
+    public String hobby;
+    public String wechat;
+    public int age;
+
+    public profile_print(String name, String country, String hobby, String wechat, int age){
+        this.name = name;
+        this.country = country;
+        this.hobby = hobby;
+        this.wechat = wechat;
+        this.age = age;
+    }
+
+    public void run(){
+        System.out.println("your profile_print:\nname: "+name+"\ncountry: "+country+"\nhobby: "+hobby+"\nwechat: "+wechat+"\nage: "+age);
+    }
+
+    public static void main(String[] arg){
+        Scanner reader = new Scanner(System.in);
+        String name = null;
+        String country = null;
+        String hobby = null;
+        String wechat = null;
+        int age = 0;
+
+        System.out.println("what is your name: ");
+        while(reader.hasNext()) {
+            name = reader.nextLine();
+            System.out.println("which country you are: ");
+            country = reader.nextLine();
+            System.out.println("what is your hobby: ");
+            hobby = reader.nextLine();
+            System.out.println("your wechat id: ");
+            wechat = reader.nextLine();
+            System.out.println("how old are you");
+            age = reader.nextInt();
+            break;
+        }
+        profile_print show = new profile_print(name, country, hobby, wechat, age);
+        if(age >0 & age < 150){
+            show.run();
+        }
+        else {
+            System.out.println("the age you input is not correct, please try again");
+        }
+
+    }
+
+
+
+}
---
+++ tutorial_java_se/day2/switcher.java
@@ -0,0 +1,23 @@
+package day2;
+import java.util.Scanner;
+
+public class switcher {
+    public int x;
+    public static void main(String[] args){
+        int x = 0;
+        System.out.println("please input a number <= 9: ");
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            x = scan.nextInt();
+        }
+        switch (x){
+            case 3: case 5: case 7:
+                System.out.println("ODD number");
+                break;
+            case 4: case 6: case 8:
+                System.out.println("even number");
+            default:
+                System.out.println("min & max number");
+        }
+    }
+}
