013aa73992e83370dd0ceed590264459fe9a71d3
ShengzhenFu <fushengzhen@163.com>
2019-05-11 17:43:31 +0800

    loop statement day07

---
+++ tutorial_java_se/day07/loop.java
@@ -0,0 +1,96 @@
+package day07;
+
+public class loop {
+	/*
+	 * while loop to print 5 stars 5 times
+	 */
+	void while_loop_print_5stars() {
+		int count = 0;
+		while(count < 5) {
+			System.out.println("   *");
+			System.out.println("  ***");
+			System.out.println(" *****");
+			System.out.println("*******");
+			System.out.println(" *****");
+			System.out.println("  ***");
+			System.out.println("   *");
+			count++;
+		}
+	}
+	/*
+	 * while loop to print 1234
+	 */
+	void while_loop_print1234(){
+		int count =0;
+		while(count<5) {
+			System.out.println(count);
+			count++;
+		}
+		System.out.println("***************************");
+	}
+	/*
+	 * for loop with break
+	 */
+	void while_loop_print12(){
+		int count =0;
+		while(count<5) {
+			System.out.println(count);
+			if(count == 2) {
+				break;  // the loop will stop at 2
+			}
+			count++;
+		}
+		System.out.println("***************************");
+	}
+	/*
+	 * do-while loop  
+	 */
+	void do_while_loop_print1234() {
+		int count =0;
+		do {
+			System.out.println(count);
+			count++;
+		}while(count<5);
+		System.out.println("***************************");
+	}
+	/*
+	 * FOR loop  
+	 */
+	void for_loop_basic() {
+		for(int i =0; i <5; i++) {
+			System.out.println(i);
+		}
+		System.out.println("***************************");
+	}
+	/*
+	 * nested for loop  
+	 */
+	void nested_for_loop() {
+		for (int i = 0; i < 3; i++) {
+			for(int j = 1; j <= 4 ; j++) {
+				System.out.println("[i: " + i + ", j: " + j + "]");
+			}
+		}
+		System.out.println("***************************");
+	}
+	/*
+	 * for loop with break
+	 */
+	void for_loop_break() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+			if(i == 5) {
+				break;  // the loop will stop at 5 
+			}
+		}
+	}
+	public static void main(String[] args) {
+		loop lp = new loop();
+		lp.while_loop_print_5stars();
+		lp.while_loop_print1234();
+		lp.do_while_loop_print1234();
+		lp.for_loop_basic();
+		lp.nested_for_loop();
+		lp.for_loop_break();
+	}
+}
